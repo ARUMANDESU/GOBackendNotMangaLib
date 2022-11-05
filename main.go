8e9d6531c498a9bf564194193404a7ca98cf3e17
Arumandesu <zh.arumandes@gmail.com>
2022-11-05 19:30:43 +0600

    home, createManga, getManga

--- main.go
+++ main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	User string `json:"user"`
+}
+
+type Manga struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func NewManga() *Manga {
+	return &Manga{Id: 0, Name: "chainsaw man", Description: "something"}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	user := User{"Arman"}
+	manga := NewManga()
+
+	resp := make(map[string]any)
+	resp["user"] = user
+	resp["manga"] = manga
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+}
+
+func createManga(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
+	w.Write([]byte("Create new manga..."))
+
+}
+
+func getManga(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	manga := NewManga()
+
+	err := json.NewEncoder(w).Encode(manga)
+	if err != nil {
+		return
+	}
+
+}
 
 func main() {
-	fmt.Print("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/manga/create", createManga)
+	mux.HandleFunc("/manga/", getManga)
+
+	log.Print("Starting server on :5000")
+	err := http.ListenAndServe(":5000", mux)
+	log.Fatal(err)
 }
